f9c9e6a1dc8a5ead8e6ab747828f4d04e9b41b52
a7med-ab <ahmedabdulaziz1147ab@gmail.com>
2025-04-15 17:09:15 +0200

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 
 const connectionDB=async()=>{
-    return await mongoose.connect("mongodb+srv://ahmedabdulaziz1147ab:admin@cluster0.mvsqt7x.mongodb.net/virus_appV2?retryWrites=true&w=majority&appName=Cluster0")
+   return await mongoose.connect("mongodb+srv://ahmedabdulaziz875:admin@cluster0.tq777ei.mongodb.net/project?retryWrites=true&w=majority&appName=Cluster0")
     //"mongodb+srv://ahmedabdulaziz1147ab:admin@cluster0.nz6ic.mongodb.net/virus_app?retryWrites=true&w=majority&appName=Cluster0") // /naMeOFdB
     
     .then(()=>{
